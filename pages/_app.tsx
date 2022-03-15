fe77db2cc073ff8773df8cacb70ce9acb82be780
blangka <chaehong73@gmai.com>
2022-03-15 21:49:39 +0900

    자동 로그인 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import App, { AppContext, AppProps } from "next/app";
 import axios from "../lib/api";
 import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
-import { wrapper } from "../store";
+import {useSelector, wrapper } from "../store";
 import { cookieStringToObject } from "../lib/utils";
 import { meAPI } from "../lib/api/auth";
 import { userActions } from "../store/user";
@@ -22,18 +22,16 @@ app.getInitialProps = async (context: AppContext) => {
     const appInitialProps = await App.getInitialProps(context);
     const cookieObject = cookieStringToObject(context.ctx.req?.headers.cookie);
     const { store } = context.ctx;
-    if (store) {
-        const { isLogged } = store.getState().user;
-        try {
-            if (!isLogged && cookieObject.access_token) {
-                const headers = axios.defaults.headers as any;
-                headers.cookie = cookieObject.access_token;
-                const { data } = await meAPI();
-                store.dispatch(userActions.setLoggedUser(data));
-            }
-        } catch (e:any) {
-            console.log(e.message);
+    const { isLogged } = context.ctx.store.getState().user;
+    try {
+        if (!isLogged && cookieObject.access_token) {
+            const headers = axios.defaults.headers as any;
+            headers.cookie = cookieObject.access_token;
+            const { data } = await meAPI();
+            store.dispatch(userActions.setLoggedUser(data));
         }
+    } catch (e:any) {
+        console.log(e.message);
     }
     return { ...appInitialProps };
 };
