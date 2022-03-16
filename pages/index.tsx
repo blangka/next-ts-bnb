bc8230fdda06e17065bfcd3164946baa058ce34b
blangka <chaehong73@gmai.com>
2022-03-16 16:57:35 +0900

    숙소 메인 화면 구성 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { NextPage } from "next";
+import Home from "../components/home/Home";
 
 const index: NextPage = () => {
-    return <div>hello</div>;
+    return <Home />;
 };
 
 export default index;
